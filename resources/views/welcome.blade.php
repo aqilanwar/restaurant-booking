de26b2f215dfa0b18249b989b4537a4c195906a8
Aqil Anwar <aqilanwarx@gmail.com>
2023-05-23 23:07:12 +0800

    contact us

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -60,12 +60,10 @@
             <div class="text-center">
                 <h1 class="text-4xl font-bold text-gray-900 mt-8 " id="view-menu">Indulge in Exquisite Culinary Delights</h1>
                 <h2 class="mt-4 text-lg text-gray-700 mb-8">Explore our menu and discover delicious dishes.</h2>
-
             </div>
             
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
                 <!-- Card 1 -->
-
                 @foreach ($data as $food)                    
                 <div class="card w-96 glass">
                     <figure>
@@ -76,8 +74,62 @@
                       <p>RM{{ number_format($food->price,2) }}</p>
                     </div>
                 </div>
-    
                 @endforeach
+            </div>
+        </div>
+
+        <div class="container mx-auto py-8 w-full" >
+            <div class="text-center">
+                <h1 class="text-4xl font-bold text-gray-900 mt-8 " id="view-menu">Contact us</h1>
+                <h2 class="mt-4 text-lg text-gray-700 mb-8">Come and enjoy your coffee!</h2>
+                <div class="flex">
+                    <div class="w-64 bg-white rounded-lg shadow-md p-4 mx-auto">
+                        <div class="text-center"><a href=""></a>
+                            <h1 class="text-2xl font-bold text-gray-900">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline-block">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                                  </svg>
+                                  Address</h1>
+                            <p class="text-gray-700">D1-03-03 Tamarind Square, Persiaran Multimedia, 63000 Cyberjaya, Selangor</p>
+    
+                        </div>
+                      </div>
+                    <div class="w-64 bg-white rounded-lg shadow-md p-4 mx-auto">
+                        <div class="text-center">
+                            <h1 class="text-2xl font-bold text-gray-900">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline-block">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+                                  </svg>
+                                  
+                                Phone Number
+                                
+                            </h1>
+
+                          <p class="text-gray-700">+6013-266-2780
+                        </p>
+    
+                        </div>
+                      </div>
+                    <div class="w-64 bg-white rounded-lg shadow-md p-4 mx-auto">
+                        <div class="text-center">
+                            <h1 class="text-2xl font-bold text-gray-900">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline-block">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+                                </svg>
+                                Email</h1>
+
+                          <p class="text-gray-700">ontact@richiamo.com</p>
+    
+                        </div>
+                      </div>
+
+                </div>
+                  
+                <div class="mx-auto py-8">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3984.6368964213802!2d101.64011181210083!3d2.9203348045010933!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cdb7d60e01fd99%3A0xf2e164b9ea267293!2sRichiamo%20Cyberjaya!5e0!3m2!1sen!2smy!4v1684852635400!5m2!1sen!2smy" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+
             </div>
         </div>
     </body>
